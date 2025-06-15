e8ef3a89fd525aa1e30c0e0e8eed97a925bba7b1
Wilson Costa <wilson.costa@votorantim.com>
2025-06-15 16:53:40 -0300

    fix: update path in app

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = FastAPI(
     title='API de Extração de Informações de Boletos Bancários de Seguradoras'
     ,version='1.0.0'
     ,summary='Esta API permite o envio de múltiplos boletos bancários em formato digital, facilitando a extração automatizada de dados relevantes. Projetada para otimizar a gestão de documentos financeiros, a ferramenta realiza a leitura e interpretação dos boletos, retornando as informações de forma estruturada e acessível.'
+    ,openapi_url='/api/v1/openapi.json'
+    ,docs_url='/api/v1/docs'
+    ,redoc_url='/api/v1/redoc'
+    ,root_path='/finance'
 )
 
 app.include_router(API_ROUTER)
